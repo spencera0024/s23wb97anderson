f40e73b0c3caf81feed73fba745eadf25692a9b2
spencera0024 <spencera024@gmail.com>
2023-03-31 16:55:44 -0500

    Added a new endpoint for gems

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gemsRouter = require('./routes/gems');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/gems', gemsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
